4c98848e6404508b6d1460755b4a18bcb9bda73a
Jayromberg <jayro.dev@gmail.com>
2023-05-17 14:12:40 -0300

    feat: Box component

---
+++ src/components/Box/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BaseComponent } from "@src/theme/BaseComponent";
+import { StyleSheet } from "@src/theme/StyleSheet";
+
+interface BoxProps {
+  tag: 'main' | 'div' | 'article' | 'section' | 'ul';
+  children?: React.ReactNode;
+  styleSheet?: StyleSheet;
+}
+
+export default function Box({ styleSheet, tag, children, ...props }: BoxProps) {
+  return (
+    <BaseComponent as={tag} styleSheet={styleSheet} {...props}>
+      {children}
+    </BaseComponent>
+  );
+}
+
+Box.defaultProps = {
+  tag: 'div',
+}
